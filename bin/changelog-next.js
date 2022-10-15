bd9a5e7475b1c4085e74be22b12e5050e4f228bb
Glenn Fowler <glenn@cebjyre.com>
2022-10-15 20:07:53 +1100

    Make functional code
    This seems to do the job, so that's nice

--- bin/changelog-next.js
+++ bin/changelog-next.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
-import { getVersion } from "../lib/index.js";
+import { getTemplate, getVersion, generateChangelog, updateTemplate, writeChangelog, writeTemplate } from "../lib/index.js";
 
-console.log(await getVersion())
+const version = await getVersion();
+const template = await getTemplate();
+const now = new Date();
+const currDate = `${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()}`;
+
+writeTemplate({template: updateTemplate({template, version, date: currDate})});
+writeChangelog({changelog: generateChangelog({template, version, date: currDate})});
