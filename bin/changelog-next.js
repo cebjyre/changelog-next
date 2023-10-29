8f39bc6a5e6e1ad2d23bb115d5e5debcb87fe608
Glenn Fowler <glenn@cebjyre.com>
2023-10-30 00:04:26 +1100

    Allow changelog filename to be supplied
    Provide a '-o filename' option to allow markdown filenames (maintaining
    'CHANGELOG' as default)

--- bin/changelog-next.js
+++ bin/changelog-next.js
@@ -2,10 +2,15 @@
 
 import { getTemplate, getVersion, generateChangelog, updateTemplate, writeChangelog, writeTemplate } from "../lib/index.js";
 
+let filename = 'CHANGELOG';
+if(process.argv[2] === '-o') { // If arguments get trickier, use something smarter
+	filename = process.argv[3]
+}
+
 const version = await getVersion();
 const template = await getTemplate();
 const now = new Date();
 const currDate = now.toISOString().split('T')[0];
 
 writeTemplate({template: updateTemplate({template, version, date: currDate})});
-writeChangelog({changelog: generateChangelog({template, version, date: currDate})});
+writeChangelog({filename, changelog: generateChangelog({template, version, date: currDate})});
